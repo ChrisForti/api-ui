af32a10e2684c33aef4b6593cb4364df324e1fff
Christopher Forti <christopher.forti.79@gmail.com>
2025-01-22 19:50:58 -0500

    Created a login form.

--- src/api.ts
+++ src/api.ts
@@ -15,3 +15,17 @@ export async function getSnippetById(id: number) {
   return data;
 }
 // if you need a post request, you have to pass in a options object with a method of post.
+
+export async function login(email: string, password: string) {
+  const endpoint = apiUrl + "/login";
+  const loginData = {
+    email,
+    password,
+  };
+  // need some validation for username and password
+
+  const response = await fetch(endpoint, {
+    method: "POST",
+    body: JSON.stringify(loginData),
+  });
+}
---
+++ src/components/LoginForm.tsx
@@ -0,0 +1,20 @@
+import { Input } from "./Input";
+
+export function LoginForm() {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+  return (
+    <form className="ml-4 pt-4" onSubmit={handleSubmit}>
+      <div className="pt-5">
+        <Input />
+        <button
+          type="submit"
+          className="mt-4 rounded bg-blue-900 px-4 py-2 font-bold text-white hover:bg-red-500 focus:outline-none"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+}
