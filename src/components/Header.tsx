da9d7ec6b1ba49fc811dc897de631e320dd310f2
Christopher Forti <christopher.forti.79@gmail.com>
2025-01-23 20:02:01 -0500

    Installed react router, and implemented Link for login link.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,13 @@
+import { Link } from "react-router-dom";
+
 export function Header() {
   return (
     <div className="bg-slate-200 p-6">
       <header className="text-center text-7xl text-blue-900">
         Express Snippet Box
+        <Link to="/login" className="ml-4 text-blue-500 underline">
+          Login
+        </Link>
       </header>
     </div>
   );
